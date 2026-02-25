ab2138aa2be55ad3d41c98e144bb83bf81dd39fc
VitorHAlves <vhssantos2005@gmail.com>
2026-02-24 22:00:59 -0300

    Exercicio 9 concluído

---
+++ Lista de Exercicio 2/Exercicio 9.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 9</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label fw-bold">Digite o raio do círculo: </label>
+                <input type="number" id="raio" name="raio" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Calcular a área</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST")//$_SERVER é um array, por isso usa [], quero a posição REQUEST_METHOD.
+        {//método usado para requisição.
+            $raio = $_POST['raio'];//leio 1 campo do formulario
+            $area = $raio ** 2 * 3.14;
+            echo "A área do circulo é: $area";
+        }
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>

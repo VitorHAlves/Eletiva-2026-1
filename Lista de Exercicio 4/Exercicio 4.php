e009224c21bca6bf060a300bd62a675ebb580ce9
VitorHAlves <vhssantos2005@gmail.com>
2026-03-10 20:34:34 -0300

    Exercicio 4 da lista de Exercicio 4 concluido

---
+++ Lista de Exercicio 4/Exercicio 4.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label fw-bold">Digite um dia: </label>
+                <input type="text" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="num1" class="form-label fw-bold">Digite um mês: </label>
+                <input type="text" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="num1" class="form-label fw-bold">Digite um ano: </label>
+                <input type="text" id="valor3" name="valor3" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Avançar</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+            $dia = $_POST['valor1'];
+            $mes = $_POST['valor2'];
+            $ano = $_POST['valor3'];
+            if(checkdate($mes,$dia,$ano))
+            {
+                echo "Data válidada! a data é: $dia/$mes/$ano";
+            }
+            else
+            {
+                echo "Data inválida!";
+            }
+        }
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>

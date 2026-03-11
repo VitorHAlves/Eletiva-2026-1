e84c040efc20156006ce1a19a2358feb043cae44
VitorHAlves <vhssantos2005@gmail.com>
2026-03-10 21:52:30 -0300

    Exercicio 8 da lista de exercicio 4

---
+++ Lista de Exercicio 4/Exercicio 8.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 8</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label fw-bold">Digite uma frase: </label>
+                <input type="text" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Contar</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+            $valor1 = $_POST['valor1'];
+            $qntdV = substr_count($valor1,"a") + 
+            substr_count($valor1,"e") +
+            substr_count($valor1,"i") +
+            substr_count($valor1,"o") + 
+            substr_count($valor1,"u");
+            echo "A quantidade de vogais na palavra é : $qntdV";
+                
+        }
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>

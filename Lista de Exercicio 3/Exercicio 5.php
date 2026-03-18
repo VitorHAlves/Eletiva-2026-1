e1aa117bea6728f790a0e4d8cde33764a4316ded
Vitor Hugo <vhssantos2005@gmail.com>
2026-03-18 13:36:28 -0300

    Exercicio 5 da lista de exercicio 3 concluido

---
+++ Lista de Exercicio 3/Exercicio 5.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 5</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label fw-bold">Digite um número de um mês: </label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Avançar</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+            $valor1 = $_POST['valor1'];
+            
+            switch($valor1)
+            {
+                case 1:
+                    echo "Janeiro";
+                    break;
+
+                case 2:
+                    echo "Fevereiro";
+                    break;
+                case 3:
+                    echo "Março";
+                    break;
+                case 4:
+                    echo "Abril";
+                    break;
+                case 5:
+                    echo "Maio";
+                    break;
+                case 6:
+                    echo "Junho";
+                    break;
+                case 7:
+                    echo "Julho";
+                    break;
+                case 8:
+                    echo "Agosto";
+                    break;
+                case 9:
+                    echo "Setembro";
+                    break;
+                case 10:
+                    echo "Outubro";
+                    break;
+                case 11:
+                    echo "Novembro";
+                    break;
+                case 12:
+                    echo "Dezembro";
+                    break;
+
+                default:
+                    echo "Valor inválido! Digite apenas valor de 1 a 12.";
+                    break;
+            }
+            
+        }
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>

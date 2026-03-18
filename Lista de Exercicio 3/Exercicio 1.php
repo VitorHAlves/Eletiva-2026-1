17968d1c8c21028c77b399b974f26d83b0b37b05
VitorHAlves <vhssantos2005@gmail.com>
2026-03-17 21:06:26 -0300

    Exercicio 1 da lista de Exercicio 3 arurmado

--- Lista de Exercicio 3/Exercicio 1.php
+++ Lista de Exercicio 3/Exercicio 1.php
@@ -45,16 +45,43 @@
     if ($_SERVER['REQUEST_METHOD'] == "POST")
     {
         $menor = $_POST['valor1'];
+        $posicao = 1;
         $valor2 = $_POST['valor2'];
         $valor3 = $_POST['valor3'];
         $valor4 = $_POST['valor4'];
         $valor5 = $_POST['valor5'];
         $valor6 = $_POST['valor6'];
         $valor7 = $_POST['valor7'];
-        $posicao = 1;
-        for($i=1;$i<=7;$i++)
-        {
-        }
+        if ($valor2 < $menor)
+            {
+                $menor = $valor2;
+                $posicao = 2;
+            }
+        if ($valor3 < $menor)
+            {
+                $menor = $valor3;
+                $posicao = 3;
+            }
+        if ($valor4 < $menor)
+            {
+                $menor = $valor4;
+                $posicao = 4;
+            }
+        if ($valor5 < $menor)
+            {
+                $menor = $valor5;
+                $posicao = 5;
+            }
+        if ($valor6 < $menor)
+            {
+                $menor = $valor6;
+                $posicao = 6;
+            }
+        if ($valor7 < $menor)
+            {
+                $menor = $valor7;
+                $posicao = 7;
+            }
 
         echo "<p>Menor valor: $menor</p>";
         echo "<p>Posição: $posicao</p>";

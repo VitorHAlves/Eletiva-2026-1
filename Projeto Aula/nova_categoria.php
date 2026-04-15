ba0fc4396c82a83b71398f18f39f884641d4f1af
VitorHAlves <vhssantos2005@gmail.com>
2026-04-14 21:54:44 -0300

    aula 14-04

--- Projeto Aula/nova_categoria.php
+++ Projeto Aula/nova_categoria.php
@@ -5,11 +5,34 @@
 <h1>Nova Categoria</h1>
 <form method="post">
     <div class="mb-3">
-        <label for="descricao" class="form-label">Informe a descrição</label>
-        <input type="text" id="descricao" name="descricao" class="form-control" required="">
+        <label for="nome" class="form-label">Informe o nome</label>
+        <input type="text" id="descricao" name="nome" class="form-control" required="">
     </div>
     <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        require_once('conexao.php');
+        $nome = $_POST['nome'];
+        try
+            {
+                $stmt = $conexao->prepare('INSERT INTO categorias (nome) VALUES (?);');//? -> apelido pra n colocar direto a variavle do bd
+                if($stmt->execute([$nome]))
+                {
+                    echo "<p>Cadastro realizado! Faça o login!</p>";
+                }
+                else
+                {
+                    echo "<p>Erro ao cadastrar! Tente novamente</p>";
+                }
+            }
+        catch(Exception $e)
+            {
+                echo "Erro: ".$e->getMessage();
+            }
+            }
+        ?>
 
 <?php
     require_once('rodape.php');
